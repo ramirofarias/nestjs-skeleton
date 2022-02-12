f2c065d2a0441244962d61cd6efeea7313b5be8f
Ramiro Farias <ramirofarias.dev@gmail.com>
2022-02-12 14:59:46 -0300

    Added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
